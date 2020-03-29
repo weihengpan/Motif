cee77b229dc4eaace7c61ead03e9185a849e73ec
Weiheng Pan <waynepan@link.cuhk.edu.hk>
2020-03-29 14:23:52 +0800

    Initial Commit

---
+++ Motif/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Motif
+//
+//  Created by 潘维恒 on 2020/3/29.
+//  Copyright © 2020 PAN Weiheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MotifTests/MotifTests.swift
@@ -0,0 +1,34 @@
+//
+//  MotifTests.swift
+//  MotifTests
+//
+//  Created by 潘维恒 on 2020/3/29.
+//  Copyright © 2020 PAN Weiheng. All rights reserved.
+//
+
+import XCTest
+@testable import Motif
+
+class MotifTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
